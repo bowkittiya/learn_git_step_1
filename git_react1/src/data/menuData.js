f47dd07affce69a44ed6ed165d3efe18b5ddaad4
bowkittiya <popittt4750@gmail.com>
2025-06-13 11:47:51 +0700

    update

---
+++ git_react1/src/data/menuData.js
@@ -0,0 +1,15 @@
+export const menuItems =[{
+    id:1,
+    name:"ข้าวหมูทอด",
+    price:50,
+    image:"https://media.timeout.com/images/105621312/image.jpg",
+    desc:"ช้าวหมูทอดหอมฉุย"
+    
+   } ,
+   {id:2,
+    name:"ข้าวผัดกุ้ง",
+    price:55,
+    image:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg7p2pDrQ77E07Lue_loEfmaTm9dmjjBl2k2X0WeJwqA05141Q8kSbURZeKtBt0etttVwzzD72l1YHzkzQ175cUTK37H9nvpfC75VB_YtYCnGIj_PUN9Qo_fuEGOIdz1aeGBFFQ5Lh8VIrm/w1200-h630-p-k-no-nu/987663.jpg",
+
+   },
+]
